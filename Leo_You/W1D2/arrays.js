39a777306d738ba71fce983b51a9fb495701e426
yehong you <youyehong@gmail.com>
2016-03-16 19:56:40 +1100

    week 1 day two works finished~

---
+++ Leo_You/W1D2/arrays.js
@@ -0,0 +1,33 @@
+//your top choice
+
+var colors =["blue","white","orange"];
+for (var index = 1; index <= colors.length; index++){
+  console.log("My #" + index + " colors choice is "+ colors[index-1]);
+}
+
+//the even/odd reporter 0~20
+
+for (var num = 0; num <= 20; num++){
+     if (num%2 === 0){
+       console.log(num +' is even !');
+     }else{
+       console.log(num +' is odd !');
+     }
+}
+
+//Multiplication Tables
+
+for (var b = 1; b < 10; b++){
+    for (var a= 1; a < 10; a++){
+        console.log(a +" * "+ b +" = "+ a*b );
+    }
+}
+//The Vegan Test
+var choose = prompt("Them menu tonight is Steak, fruit salad, tofurkey, pork chops").toUpperCase();
+if (choose === "STEAK" || choose === "PORK CHOPS"){
+  console.log("Vegans beware!");
+}else if (choose === "FRUIT" || choose === "TOFURKEY"){
+  console.log("This cuisine is vegan friendly!");
+}else {
+  console.log("please choose one disk of menu !!");
+}
