21f913ab004c549dc2222e6501629c732565612b
Katy Dattoma <katy.dattoma@gmail.com>
2016-03-17 16:58:45 +1100

    matt is awesome

--- katy_dattoma/loops.js
+++ katy_dattoma/loops.js
@@ -1,4 +1,4 @@
-console.log('Best IceCream EVER');
+/*console.log('Best IceCream EVER');
 
  
 var favIceCream = ["strawberries","vanilla","choc chip","rocher"];
@@ -13,17 +13,48 @@ console.log('My #4 choice is ' + favIceCream[3]);
 //is even or odd,
 // and report that to the screen (e.g. "2 is even").
 
-var currentNumber = 44;
+
 
 for(var i = 0; i < 20 ;i++){
+	console.log(i);
 
 if(currentNumber % 2 === 0){
-	console.log("It's even!");
+	console.log(i + "is even!");
 }
 else{
-	console.log("It's odd");
+	console.log(i + " is odd");
 }
 
+} */
+
+console.log ('choices');
+
+var simpson = ['homer', 'lisa', 'bart', 'maggie', 'ned'];
+
+var suffix = ['st', 'nd', 'rd', 'th']
+
+for(var i =0; i < simpson.length; i++) {
+	console.log(simpson.length[i]);
+	console.log(suffix[i]);
+}
+
+
+//if you wanna use a while loop you need a prompt from the user.
+// Just be careful cause the prompt will always turn in strings, so if you have 
+// to compare your prompt to a number you need to transform the prompt in number as you can
+// see below.
+
+console.log('Guessing game');
+
+var secretNumber = 42;
+
+var guess = Number( prompt('Guess a number between 1 and 5') );
+
+while (guess !== secretNumber) {
+	console.log('you fail life');
+	console.log('guess' , guess);  //just to display your guess 
+	guess = Number (prompt('guess a number between 1 and 5') );
+		
 }
 
 /*Write a for loop that will iterate from 0 to 10. 
@@ -47,6 +78,15 @@ for(i = 0; i <= 10; i++) {
 
 }
 
+for (i = 1; i <=10 ; i++) {
+	console.log('9 x ' + i + ' = '+ 9 * 1);
+
+//this loop goes 10 times per the times that my preview loop goes
+	for (var j = 1; j <= 10 ; j++) {
+		console.log(j + ' x ' + i + ' = ' + j*i);
+	}
+
+}
 
 /*On the menu tonight: Steak, fruit salad, 
 tofurkey, pork chops.
@@ -64,7 +104,7 @@ Hint: Two identical strings are considered to be equal
 to each other.*/
 
 
-
+/*
 var userChoice = prompt("What is gonna be your choice?");
 
 
@@ -83,7 +123,19 @@ if (userChoice === 'tofurkey') {
 // else {
 // 	console.log('Vegans beware!');
 // }
+*/
 
+var menu = ['steak', 'pork chops', 'fruit salad','tofurkey'];
+
+var choice = 'steak';
+
+if (choice === 'steak' || choice === 'pork chops') {
+
+	console.log('meat');
+}
+else {
+	conosole.log('vegan');
+}
 
 
 
