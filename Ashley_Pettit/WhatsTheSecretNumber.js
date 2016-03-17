7d5e9ca534921bb667fc1e5e67773ea136dc0f90
Ashleypettit <ashley@digital380.com.au>
2016-03-18 08:36:21 +1100

    adding homework from 17th march

--- Ashley_Pettit/WhatsTheSecretNumber.js
+++ Ashley_Pettit/WhatsTheSecretNumber.js
@@ -1,8 +1,8 @@
 function guess_the_number(secretNumber) {
 
-    while (secretNumber != user_choice) {
-        var user_choice = prompt("What is the secret number");
-        if (user_choice == secretNumber) {
+    while (secretNumber !== user_choice) {
+        user_choice = Number (prompt("What is the secret number")) ;
+        if (user_choice === secretNumber) {
             break;
         }
         else {
@@ -11,11 +11,6 @@ function guess_the_number(secretNumber) {
     }
 }
 
-//guess_the_number(42);
+guess_the_number(42);
 
-function set_the_number(){
-	return prompt("Set the secret number as what?");
-}
-
-var number = set_the_number;
-alert(number);
+.freeze() 
